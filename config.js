b82ac0074a0352b11858a66e8f2e70c54ed16ce9
Akash <akashshekara1212@gmail.com>
2023-09-30 18:34:02 -0500

    adding evilpuppet files

--- config.js
+++ config.js
@@ -1,18 +1,5 @@
-
-const path = require('path');
-
-const CACHED_RESOURCES_DIR = path.resolve('cached_resources');
-const BASE_URL = 'https://google.com/';
-const PORT = 3000;
-
-const CONTENT_URL = `http://localhost:${PORT}/getContent?url=`;
-
-
-
+const PORT = 5000;
 
 module.exports = {
-    CACHED_RESOURCES_DIR,
-    BASE_URL,
-    CONTENT_URL,
-    PORT,
+    PORT
 }
